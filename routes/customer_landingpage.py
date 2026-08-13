5d3487c1d2023be395655c63727aa7825d97949a
Jron068 <aronjohncastillo068@gmail.com>
2026-08-13 22:59:59 +0800

    Update customer_landingpage.py

--- routes/customer_landingpage.py
+++ routes/customer_landingpage.py
@@ -1,6 +1,17 @@
-from flask import render_template
+from flask import render_template, session
 from app import app
 
 @app.route("/customer_landingpage")
 def customer_landingpage():
-    return render_template("customer/customer_landingpage.html")
+    fullname = session.get("fullname", "Guest")
+    return render_template("customer/customer_landingpage.html", username=fullname)
+
+@app.route("/customer_aboutpage")
+def customer_aboutpage():
+    fullname = session.get("fullname", "Guest")
+    return render_template("customer/customer_aboutpage.html", username=fullname)
+
+@app.route("/customer_howitworks")
+def customer_howitworks():
+    fullname=session.get("fullname", "Guest")
+    return render_template("customer/customer_howitworks.html", username=fullname)
